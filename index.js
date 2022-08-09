26b49374869eaf3edf8fa94908caf6afb494f3a9
Poison512 <adrian512@Adrians-MBP.attlocal.net>
2022-08-08 23:06:27 -0500

    just need to add logout func

---
+++ index.js
@@ -0,0 +1,98 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
+
+const initializePassport = require('./passport-config')
+initializePassport(passport, 
+    email => users.find(user => user.email === email ),
+    id => users.find(user => user.id === id ),
+    )
+   
+//const path = require('path');
+//const exphbs = require('express-handlebars');
+//const hbs = exphbs.create({});
+
+
+
+
+//just to test
+const users = []
+//const PORT = process.env.PORT || 3000;
+
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
+
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'view')));
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUnintialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {name: req.user.name})
+});
+//login
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login',checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+//register
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) =>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    }catch{
+        res.redirect('/register')
+    }
+})
+
+function checkAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return next;
+    }
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated (req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+app.listen(3000)
+//app.listen(PORT, () => {
+    //console.log('Server listening on: http://localhost:' + PORT);
+  //});
