aec748ad4db74ebc660c0cfd46387926f201c9a6
Kaysie <kaysielandersonwork@gmail.com>
2022-08-09 17:21:31 -0500

    created index.js in controllers, added more dependencies in server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // dependencies
 const express = require('express');
 const sequelize = require("./config/connection");
+const routes = require('./controllers/');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
