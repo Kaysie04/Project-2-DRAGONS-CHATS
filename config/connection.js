2ee3d6be3b74fc76e4a551d86cc1d43f242cc4b7
Poison512 <adrian512@Adrians-MBP.attlocal.net>
2022-08-11 20:50:59 -0500

    added setting to edit user and password

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, "", {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
   host: '127.0.0.1',
   dialect: 'mysql',
   port: 3306
